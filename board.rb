1b47dcc280030b3717971f341bb3f67489da2aff
Mina Doroudi <mina.doroudi@gmail.com>
2012-08-05 01:07:26 -0700

    args + starting to crawl

---
+++ board.rb
@@ -0,0 +1,34 @@
+class Board 
+ def initialize
+
+ end
+
+ def to_json
+
+ end
+
+ def field_id
+
+ end
+
+ def user_id
+
+ end
+
+ def user_name
+
+ end
+
+ def slug
+
+ end
+
+ def description
+
+ end
+
+ def category
+
+ end
+
+end 
--- get_boards.rb
+++ get_boards.rb
@@ -2,20 +2,45 @@ require 'active_record'
 require 'rubygems'
 require 'yaml'
 #require 'schema'
-
+require 'nokogiri'
+require 'open-uri'
+require 'debugger'
 class BoardCrawler 
 
-  # The seed is a users user name
-  def from_seed(seed)
-    
+  def initialize(seed = nil)
+    if !seed.nil?
+      @seed = seed
+    end
   end
 
-  def from_main_page
+  # The seed is a users user name
+  def crawl_from_seed
+    puts "begining to crawl!"
+    doc = Nokogiri::HTML(open(url(@seed)))
+    users_boards = doc.css("#wrapper.BoardLayout li")
+    users_boards.each do |board|
+      get_board_info(board)
+    end
+  end
+
+  def crawl_from_main_page
 
   end
 
   def get_board_info(board)
-
+    debugger
+    puts "boar!d"
+  end
+  
+  def url(username)
+    "http://pinterest.com/#{username}"
   end
 
+end
+
+if ARGV.size == 0
+else
+  puts "crawling the boards for #{ARGV[0]}"
+  crawler = BoardCrawler.new(ARGV[0])
+  crawler.crawl_from_seed
 end
