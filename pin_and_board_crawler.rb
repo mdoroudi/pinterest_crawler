e2693d0fc155013c80b7cac26ed1a4cb7daecc83
Mina Doroudi <mina.doroudi@gmail.com>
2012-10-13 10:51:39 -0700

    code changes to append all the result to file

--- pin_and_board_crawler.rb
+++ pin_and_board_crawler.rb
@@ -9,6 +9,7 @@ class PinAndBoardCrawler < PinterestCrawler
     @pins = []
     @boards_file = File.new("boards.json", file_mode)
     @pins_file = File.new("pins.json", file_mode)
+    @deep = false
   end
 
   def crawl_from_seed
@@ -18,10 +19,11 @@ class PinAndBoardCrawler < PinterestCrawler
     users_boards.each do |board_thumb_html|
       get_board_and_pins(board_thumb_html)
     end
-    save_to_files
+    save_to_files unless @deep
   end
 
   def crawl_from_main_page(deep = false)
+    @deep = deep
      @current_user_slug = nil 
      home_page = get_page_html("http://pinterest.com/") 
      pins = home_page.css("#wrapper #ColumnContainer .pin")
@@ -48,78 +50,6 @@ class PinAndBoardCrawler < PinterestCrawler
     board.field_id    = board_thumb_html["id"].gsub("board","")
     board.slug        = board_thumb_html.css("h3 a").first["href"].gsub( @current_user_slug, "").gsub("\/","")
     board.name        = board_thumb_html.css("h3 a").first.text
-    @users_pin_board  = get_page_html(users_url+board.slug)
-    board.description = @users_pin_board.css("#BoardDescription").text
-    board.category    = @users_pin_board.css('meta[property="pinterestapp:category"]').attr("content").value
-
-    board
-  end 
-
-  def get_pins_info(pins_html, args = {})
-    default_args = {
-      board_id: nil, 
-      slug: nil, 
-      crawling_from_main_page: "none",
-    }
-    args = default_args.merge(args)
-    pins_html.each_with_index do |pin_html, index|
-      begin
-        if args[:crawling_from_main_page] == "boards"
-          @current_user_slug = pin_html.css(".convo a").attr("href").value.split("/")[1] 
-          crawl_from_seed
-        elsif args[:crawling_from_main_page] == "pins"
-          get_pin_info_only_from_main(pin_html, index)
-        else
-          get_pin_info_from_board(pin_html, index, args)
-        end
-      rescue Exception => e
-        puts e
-        puts "There was a problem with the current pin:".red
-        puts "#{pin_html}"
-        next
-      end
-    end
-    @pins
-  end
-
-  def crawl_from_seed
-    users_page =  users_page(@current_user_slug)
-    sleep rand(1.0..2.0)
-    users_boards = users_page.css("#wrapper.BoardLayout li")
-    users_boards.each do |board_thumb_html|
-      get_board_and_pins(board_thumb_html)
-    end
-    save_to_files
-  end
-
-  def crawl_from_main_page(deep = false)
-     @current_user_slug = nil 
-     home_page = get_page_html("http://pinterest.com/") 
-     pins = home_page.css("#wrapper #ColumnContainer .pin")
-     if deep
-       get_pins_info(pins, {crawling_from_main_page: 'boards'})
-     else
-       get_pins_info(pins, {crawling_from_main_page: 'pins'})
-     end
-     save_to_files
-  end
-
-
-  def get_board_and_pins(board_thumb_html)
-    @boards << get_board_info(board_thumb_html)
-    get_pins_info(@users_pin_board.css(".pin"), {board_id: @boards.last.field_id, slug: @boards.last.slug})
-  end 
-
-  def get_board_info(board_thumb_html)
-    board_thumb_html = board_thumb_html.css(".pinBoard").first
-    board = Board.new
-        
-    board.user_name   = @current_user_slug 
-    board.user_id     = Zlib.crc32 @current_user_slug
-    board.field_id    = board_thumb_html["id"].gsub("board","")
-    board.slug        = board_thumb_html.css("h3 a").first["href"].gsub( @current_user_slug, "").gsub("\/","")
-    board.name        = board_thumb_html.css("h3 a").first.text
-    sleep rand(1.0..2.0)
     @users_pin_board  = get_page_html(users_url+board.slug)
     board.description = @users_pin_board.css("#BoardDescription").text
     board.category    = @users_pin_board.css('meta[property="pinterestapp:category"]').attr("content").value
@@ -160,11 +90,11 @@ class PinAndBoardCrawler < PinterestCrawler
     @boards.collect! { |board| board.to_json } 
     @pins.collect! { |pin| pin.to_json } 
 
+    @boards = JSON.generate(@boards)
+    @pins = JSON.generate(@pins)
+
     @boards_file.puts @boards unless @boards.empty?
     @pins_file.puts @pins unless @pins.empty?
-
-    @boards = []
-    @pins = []
   end 
 
   def get_pin_info_only_from_main(pin_html, index)
